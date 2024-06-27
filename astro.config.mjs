fb58ee61d48929efdbe933ff9077b419f1c52cfa
lawejo <lwj1993@gmail.com>
2024-06-27 15:05:19 +0200

    sentry

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,20 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
+import sentry from "@sentry/astro";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()],
+  integrations: [
+    react(),
+    tailwind(),
+    sentry({
+      dsn: "https://9f83a9583e9568bef401c2148d4e1393@o4507503955083264.ingest.de.sentry.io/4507503956983888",
+      integrations: [new BrowserTracing({ tracingOrigins: ["*"] })],
+      sourceMapsUploadOptions: {
+        project: "javascript-astro",
+        authToken: process.env.SENTRY_AUTH_TOKEN,
+      },
+    }),
+  ],
 });
