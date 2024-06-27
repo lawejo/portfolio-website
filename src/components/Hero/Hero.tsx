9ab76961272b4e45429b5553ff75c1324c05bbcc
lawejo <lwj1993@gmail.com>
2024-06-27 13:49:35 +0200

    class sorting

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -21,11 +21,14 @@ export default function App() {
   }, []);
 
   return (
-    <section id="home" className="intersect h-screen bg-primary1 border-t border-white/20">
-      <div className="w-full h-full flex justify-center align-end">
-        <div className="w-fit self-center mb-12 [&>*]:text-white [&>*]:mb-5">
+    <section
+      id="home"
+      className="intersect h-screen border-t border-white/20 bg-primary1"
+    >
+      <div className="align-end flex h-full w-full justify-center">
+        <div className="mb-12 w-fit self-center [&>*]:mb-5 [&>*]:text-white">
           <p className="w-fit text-3xl font-normal">I'm</p>
-          <h1 className="w-fit text-2xl md:text-5xl whitespace-nowrap font-bold">
+          <h1 className="w-fit whitespace-nowrap text-2xl font-bold md:text-5xl">
             Lasse Weber Jørgensen
           </h1>
           <h2 className="text-4xl">
